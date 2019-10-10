40d1df1e2d8c97de5b640fc646450c1e66e09250
asad ali <>
2019-10-10 12:44:05 +1100

    header component and unit tests

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -3,10 +3,10 @@ import './styles.scss';
 import logo from '../../logo.svg';
 const Header = (props) => {
     return (
-        <header>
+        <header className="headerComponent">
             <div className="wrap">
                 <div className="logo">
-                    <img src={ logo } />
+                    <img className="logoIMG" src={ logo } />
                 </div>
             </div>
 
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,6 @@
 import Enzyme from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
-
+console.log('setup tests called');
 Enzyme.configure({
     adapter: new EnzymeAdapter(),
     disableLifecycleMethods: true
